ddcbcc760df986e5e81770e384949c9fc31f26f2
gness1804 <graham.nessler@gmail.com>
2018-06-28 20:55:45 -0500

    Finally got fake function working

--- Is is Prime/ViewController.swift
+++ Is is Prime/ViewController.swift
@@ -12,10 +12,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mainOutput: UILabel!
     
+    func isItPrime(n: Int) -> Bool {
+        return true
+    }
+    
     @IBAction func doStuff(_ sender: Any) {
         if mainInput.text == "" {
             mainOutput.text = "Oops! You must enter a  valid number."
             return
+        }
+        let str = mainInput.text
+        let num = Int(str!)
+
+        if isItPrime(n: num!) {
+            mainOutput.text = "The number is prime!"
+        } else {
+            mainOutput.text = "The number is not prime."
         }
     }
     
